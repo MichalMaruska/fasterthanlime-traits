9c1828258d2f99c4a439fd1d1c1fdef1731f89c5
Michal Maruska <mmaruska@gmail.com>
2026-02-03 20:51:57 +0100

    trait
    panics, because lol is already catching the double end()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use lol_html::{element, HtmlRewriter, Settings};
+use lol_html::{element, HtmlRewriter, Settings, OutputSink};
 use std::error::Error;
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = vec![];
@@ -18,11 +19,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         },
         |c:&[u8]| output.extend_from_slice(c)
     )?;
+    process(input, &mut rewriter)?;
 
-    rewriter.write(input.as_bytes())?;
     rewriter.end()?;
 
     println!("input: {input}");
     println!("output: {}", std::str::from_utf8(&output).unwrap());
     Ok(())
+}
+
+fn process(input: &str, processor: &mut dyn Processor) -> Result<(), Box<dyn Error>> {
+    processor.write(input.as_bytes())?;
+    processor.end()?;
+
+    Ok(())
+}
+
+
+
+trait Processor {
+    fn write(&mut self, chunk: &[u8]) -> Result<(), Box<dyn Error>>;
+    fn end(&mut self) -> Result<(), Box<dyn Error>>;
+}
+
+
+impl<'h,O: OutputSink> Processor for HtmlRewriter<'h, O> {
+
+    fn write(&mut self, chunk: &[u8]) -> Result<(), Box<dyn Error>> {
+        HtmlRewriter::write(self, chunk).map_err(Into::into)
+    }
+
+    fn end(&mut self) -> Result<(), Box<dyn Error>> {
+        HtmlRewriter::end(self).map_err(Into::into)
+    }
 }
