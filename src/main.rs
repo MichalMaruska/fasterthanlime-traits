978298dfbc061cec55ce711f92b0c95c1975b5f2
Michal Maruska <mmaruska@gmail.com>
2026-02-03 18:29:49 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use lol_html::{element, HtmlRewriter, Settings};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = vec![];
+    let input = include_str!("input.html");
+
+    let mut rewriter = HtmlRewriter::try_new(
+        Settings {
+            element_content_handlers:
+            vec![
+                element!("img", |el| {
+                    el.set_attribute("loading", "lazy")?;
+                    Ok(())
+                })
+            ],
+            ..Default::default()
+        },
+        |c:&[u8]| output.extend_from_slice(c)
+    )?;
+
+    rewriter.write(input.as_bytes())?;
+    rewriter.end()?;
+
+    println!("input: {input}");
+    println!("output: {}", std::str::from_utf8(&output).unwrap());
+    Ok(())
 }
