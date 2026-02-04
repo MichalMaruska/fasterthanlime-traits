630aa259856f98fda67cc85ef8b5770bf03a4cfe
Michal Maruska <mmaruska@gmail.com>
2026-02-04 08:46:05 +0100

    the fix

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn process(input: &str, mut processor: Box<dyn Processor>) -> Result<(), Box<dyn Error>> {
+fn process(input: &str, mut processor: Box<dyn Processor + '_>) -> Result<(), Box<dyn Error>> {
     processor.write(input.as_bytes())?;
     processor.end()?;
     Ok(())
