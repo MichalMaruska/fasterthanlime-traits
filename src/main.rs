846ed7c23191cd688072c1c1b32758546fbdc57b
Michal Maruska <mmaruska@gmail.com>
2026-02-04 09:57:33 +0100

    remove type parameter, use fixed

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,11 @@ impl<W: io::Write> OutputSink for  WriterOutputSink<W> {
 }
 
 enum ProcessorImpl<W: io::Write> {
-    LazyLoading(HtmlRewriter<'static, O>),
+    LazyLoading(HtmlRewriter<'static, WriterOutputSink<W>>),
     HtmlEscape(Escaper<W>),
 }
 
-impl<O: OutputSink> ProcessorType {
+impl ProcessorType {
     fn build<'w, W: io::Write + 'w>(&self, mut output: W) -> ProcessorImpl<W> {
         match self {
             ProcessorType::LazyLoading => ProcessorImpl::LazyLoading(
